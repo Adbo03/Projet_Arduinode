64f0a3660eb643c79dc69ae13dc9e75cdfac525b
Ad Bo <adambounour003@gmail.com>
2026-05-18 16:06:19 +0200

    Calcul du temps a la microseconde près avec le PPS du module GPS afin de permettre par la suite une meilleure analyse des données et une potentielle synchronisation entre plusieurs systèmes identiques

--- CONVERSION_BIN_CSV.py
+++ CONVERSION_BIN_CSV.py
@@ -19,8 +19,8 @@ def convert_bin_to_csv():
     
     file_size = os.path.getsize(FILE_BIN)
 
-    # Taille d'un échantillon : 12 octets
-    sample_size = 12 
+    # Taille d'un échantillon : 16 octets
+    sample_size = 16
     
     with open(FILE_BIN, "rb") as bin_file, open(FILE_CSV, "w", newline='') as csv_file:
         writer = csv.writer(csv_file)
@@ -33,13 +33,17 @@ def convert_bin_to_csv():
             parts = gps_line.replace("GPS:", "").split(",")
             if len(parts) >= 3:
                 lat, lon, time_raw = parts[0], parts[1], parts[2]
+            
+            hours, minutes, seconds = time_raw.split(':')
+            real_seconds = int(seconds)
+            last_us = 0
 
         # Lecture des données binaires
         count = 0
         print("Conversion en cours...")
         
         while True:
-            # Lire 12 octets (X, Y, Z)
+            # Lire 16 octets (X, Y, Z) + microseconds
             data = bin_file.read(sample_size)
             if len(data) < sample_size:
                 break 
@@ -47,7 +51,7 @@ def convert_bin_to_csv():
             # Dépaquetage du binaire : 
             # '<' : petit-boutiste (little-endian, standard ESP32)
             # 'iii' : trois entiers signés de 4 octets (int32_t)
-            raw_x, raw_y, raw_z = struct.unpack('<iii', data)
+            raw_x, raw_y, raw_z, offset_us = struct.unpack('<iiiI', data)
 
             # Conversion en 'g'
             ax = raw_x / SCALE_FACTOR
@@ -56,8 +60,19 @@ def convert_bin_to_csv():
             pitch = math.atan2(-ax, math.sqrt(ay * ay + az * az)) * 180.0 / math.pi
             roll = math.atan2(ay, az) * 180.0 / math.pi
 
+            # On ajoute l'offset en ajustant si les micros dépassent 1 000 000
+
+            real_us = offset_us % 1000000
+            
+            if(last_us > real_us):
+                real_seconds += 1
+
+            timestamp = f"{hours}:{minutes}:{real_seconds:02d}.{real_us:06d}"
+
+            last_us = real_us
+
             # Écriture dans le CSV
-            writer.writerow([f"{ax:.3f}", f"{ay:.3f}", f"{az:.3f}", f"{roll:.2f}", f"{pitch:.2f}", lat, lon, time_raw])
+            writer.writerow([f"{ax:.3f}", f"{ay:.3f}", f"{az:.3f}", f"{roll:.2f}", f"{pitch:.2f}", lat, lon, timestamp])
             
             count += 1
             if count % 40000 == 0: # Progression toutes les 10 secondes de données à 4kHz
