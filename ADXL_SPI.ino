b04c3cc0fee3a52ef64d0f2b30594daa940b4523
Ad Bo <adambounour003@gmail.com>
2026-05-21 14:34:58 +0200

    Changement de la bibliothèque BLE pour utiliser NimBLE afin de configurer le MTU + amélioration de la précision en mode LIVE (4000 échantillons/sec) + renommage des fichiers de sauvegarde avec le temps de la mesure pour permettre le stockage de plusieurs fichiers (penser à changer le nom du fichier .bin lors de la conversion dans le code CONVERSION_BIN_CSV.py

--- ADXL_SPI.ino
+++ ADXL_SPI.ino
@@ -1,4 +1,4 @@
-#include <ArduinoBLE.h>
+#include <NimBLEDevice.h>
 #include <SPI.h>
 #include "SdFat.h"
 #include <TinyGPS++.h>
@@ -16,18 +16,29 @@
 #define TICK_US 1000000 / ODR_FREQUENCY
 #define PPS_PIN 2
 
+// Modes
+#define LIVE 0
+#define RECORD 1
+
 // Bus SPI dédié pour communiquer avec le lecteur SD
 SPIClass sdSPI(HSPI);
 
-// Structure de données (16 octets par échantillonnages)
-struct Sample {
+struct Sample_SD {
   int32_t x, y, z;
   uint32_t sub_sec_us; // Microsecondes écoulées depuis le dernier front PPS
 };
 
-// Double buffering
-const int BUF_SIZE = 32; 
-Sample buffer[2][BUF_SIZE];
+struct Sample_LIVE {
+  int32_t x, y, z;
+};
+
+// Buffers 
+const int BUF_SIZE_SD = 32;
+Sample_SD bufferSD[2][BUF_SIZE_SD];
+
+const int BUF_SIZE_LIVE = 42; 
+Sample_LIVE bufferLIVE[3][BUF_SIZE_LIVE];
+
 volatile int activeBuf = 0;
 volatile int bufIdx = 0;
 volatile bool fullFlag = false;
@@ -37,10 +48,11 @@ SdFile file;
 TinyGPSPlus gps;
 hw_timer_t * timer = NULL;
 
-volatile uint8_t currentMode = 0; 
+volatile uint8_t currentMode = LIVE; 
 volatile uint32_t pps_micros = 0;
 float lastLat = 0, lastLon = 0;
-unsigned long lastBleUpdate = 0;
+char hours[3], minutes[3], seconds[3];
+char title[25];
 
 // Adresses des registres
 const int REG_XDATA3 = 0x08;
@@ -50,41 +62,48 @@ const int REG_POWER_CTL = 0x2D;
 // Facteur d'échelle (+/- 2g) : 256000 LSB/g
 const float SCALE_FACTOR = 256000.0;
 
-BLEService adxlService("19b10000-e8f2-537e-4f6c-d104768a1214"); 
+// Objets et pointeurs BLE 
+NimBLEServer* pServer = NULL;
+NimBLECharacteristic* pRawDataChar = NULL;
+NimBLECharacteristic* pModeChar = NULL;
 
-// UUID modifiable (128 bits) 
-BLEFloatCharacteristic xAcceloChar("2A58", BLERead | BLENotify);
-BLEFloatCharacteristic yAcceloChar("2A59", BLERead | BLENotify);
-BLEFloatCharacteristic zAcceloChar("2A5A", BLERead | BLENotify);
-
-BLEFloatCharacteristic pitchChar("2A5B", BLERead | BLENotify);  // Rotation axe Y
-BLEFloatCharacteristic rollChar("2A5C", BLERead | BLENotify);   // Rotation axe X
-
-BLEByteCharacteristic modeChar("2A5D", BLERead | BLEWrite);     // mode (0: Temps Réel, 1: Enregistrement)
+volatile bool deviceConnected = false;
+volatile bool modeWritten = false;
 
 int32_t x_raw = 0;
 int32_t y_raw = 0;
 int32_t z_raw = 0;
 
-void IRAM_ATTR SD_handler() {
+SemaphoreHandle_t timerSemaphore;
 
-  if (currentMode == 1){
-    ADXL_ReadRaw();
-
-    // Calcul de l'offset exact en microsecondes par rapport à la seconde courante
-    uint32_t current_micros = micros();
-    uint32_t offset_us = current_micros - pps_micros;
-
-    buffer[activeBuf][bufIdx] = {x_raw, y_raw, z_raw, offset_us};
-    bufIdx++;
-
-    if (bufIdx >= BUF_SIZE) {
-      bufIdx = 0;
-      activeBuf = (activeBuf == 0) ? 1 : 0;
-      fullFlag = true;
+// --- GESTIONNAIRES DE CALLBACKS BLE ---
+class MyServerCallbacks: public NimBLEServerCallbacks {
+  public:
+    void onConnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo){
+      deviceConnected = true;
     }
-  }
+    void onDisconnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo, int reason){
+      deviceConnected = false;
+    }
+};
 
+class ModeCallbacks: public NimBLECharacteristicCallbacks {
+    public:
+      void onWrite(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) {
+        std::string value = pCharacteristic->getValue();
+        if (value.length() > 0) {
+          currentMode = (uint8_t)value[0];
+          modeWritten = true;
+        }
+      }
+};
+
+void IRAM_ATTR Sample_handler() {
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  xSemaphoreGiveFromISR(timerSemaphore, &xHigherPriorityTaskWoken);
+  if (xHigherPriorityTaskWoken) {
+    portYIELD_FROM_ISR();
+  }
 }
 
 void IRAM_ATTR PPS_handler() {
@@ -132,6 +151,39 @@ void writeRegister(uint8_t reg, uint8_t value) {
   digitalWrite(ADXL_CS, HIGH);
 }
 
+void ADXL_Task(void * pvParameters) {
+  while(1) {
+    if (xSemaphoreTake(timerSemaphore, portMAX_DELAY) == pdTRUE) {
+      ADXL_ReadRaw();
+
+      if(currentMode == LIVE){
+        bufferLIVE[activeBuf][bufIdx] = {x_raw, y_raw, z_raw};
+        bufIdx++;
+
+        if (bufIdx >= BUF_SIZE_LIVE) {
+          bufIdx = 0;
+          activeBuf = (activeBuf + 1) % 3;
+          fullFlag = true;
+        }
+      }
+
+      else if (currentMode == RECORD){
+        uint32_t current_micros = micros();
+        uint32_t offset_us = current_micros - pps_micros;
+
+        bufferSD[activeBuf][bufIdx] = {x_raw, y_raw, z_raw, offset_us};
+        bufIdx++;
+
+        if (bufIdx >= BUF_SIZE_SD) {
+          bufIdx = 0;
+          activeBuf = (activeBuf == 0) ? 1 : 0;
+          fullFlag = true;
+        }
+      }
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   Serial1.begin(9600, SERIAL_8N1, D0, D1); // UART GPS
@@ -153,64 +205,60 @@ void setup() {
 
   SPI.begin();
   sdSPI.begin(SCK_SD, MISO_SD, MOSI_SD, SD_CS);
-  
-  Serial.println("Bus SPI initialise.");
 
-  Serial.println("Tentative initialisation SD...");
+  Serial.println("Bus SPI initialise.\nTentative initialisation SD...");
   
-  // On utilise SdSpiConfig pour forcer la vitesse à 16MHz (ou 10MHz pour plus de stabilité)
   if (!sd.begin(SdSpiConfig(SD_CS, DEDICATED_SPI, SD_SCK_MHZ(10), &sdSPI))) {
       Serial.println("Erreur Carte SD !");
       sd.initErrorPrint(&Serial); 
       while(1);
   }
 
+  timerSemaphore = xSemaphoreCreateBinary();
+  xTaskCreatePinnedToCore(ADXL_Task, "ADXL_Task", 4096, NULL, 10, NULL, 1);
+
+  // --- CONFIGURATION BLE  ---
+  NimBLEDevice::init("ADXL355Z");
+  NimBLEDevice::setMTU(512); 
+
+  pServer = NimBLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  NimBLEService* pService = pServer->createService("19b10000-e8f2-537e-4f6c-d104768a1214");
+
+  pRawDataChar = pService->createCharacteristic(
+                   "19b10001-e8f2-537e-4f6c-d104768a1214",
+                   NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY
+                 );
+
+  pModeChar = pService->createCharacteristic(
+                "19b10002-e8f2-537e-4f6c-d104768a1214",
+                NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE
+              );
+  pModeChar->setCallbacks(new ModeCallbacks());
+
+  pService->start();
+
+  NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(pService->getUUID());
+  pAdvertising->setName("ADXL355Z");
+  pAdvertising->setPreferredParams(0x06, 0x12); 
+  pAdvertising->start();
+  
+  Serial.println("Configuration BLE terminée.");
+
+  // Configuration de la précision de l'accéléromètre (2g)
+  writeRegister(REG_RANGE, 0x01); 
+  writeRegister(REG_POWER_CTL, 0x00);
+
   // Configuration du Timer ESP32 pour le 4kHz (mode Enregistrement)
   timer = timerBegin(0, 80, true); // Diviseur 80 = 1MHz (1 tick = 1us)
-  timerAttachInterrupt(timer, &SD_handler, true);
+  timerAttachInterrupt(timer, &Sample_handler, true);
   timerAlarmWrite(timer, TICK_US, true);
   timerAlarmEnable(timer);
 
   pinMode(PPS_PIN, INPUT_PULLDOWN);
   attachInterrupt(digitalPinToInterrupt(PPS_PIN), PPS_handler, RISING);
-
-  if (!BLE.begin()) {
-    Serial.println("Echec de l'initialisation du module BLE !");
-    while (1);
-  }
-
-  // Creation du service
-  BLE.setLocalName("ADXL355Z");
-  BLE.setAdvertisedService(adxlService);
-
-  // Ajout des caractéristiques au service
-  adxlService.addCharacteristic(xAcceloChar);
-  adxlService.addCharacteristic(yAcceloChar);
-  adxlService.addCharacteristic(zAcceloChar);
-  adxlService.addCharacteristic(pitchChar); 
-  adxlService.addCharacteristic(rollChar);
-  adxlService.addCharacteristic(modeChar);
-
-  // Ajout du service
-  BLE.addService(adxlService);
-  
-  // Initialisation des charactéristiques
-  xAcceloChar.writeValue(0);
-  yAcceloChar.writeValue(0);
-  zAcceloChar.writeValue(0);
-  pitchChar.writeValue(0);
-  rollChar.writeValue(0);
-  modeChar.writeValue(currentMode);
-
-  // publication du service BLE
-  BLE.advertise();
-  Serial.println("Configuration BLE terminée.");
-
-  Serial.println("Initialisation ADXL355 (SPI)...");
-
-  // Configuration de la précision de l'accéléromètre (2g)
-  writeRegister(REG_RANGE, 0x01); 
-  writeRegister(REG_POWER_CTL, 0x00);
 
   Serial.println("Système prêt.");
 }
@@ -227,91 +275,97 @@ void loop() {
     }
   }
 
-  BLEDevice central = BLE.central();
+  if (deviceConnected) {
+      
+      if(modeWritten){
+        modeWritten = false;
 
-  if (central) {
-    if (central.connected()){
-      if(modeChar.written()){
-        currentMode = modeChar.value();
-
-        if(currentMode == 0){
+        if(currentMode == LIVE){
           Serial.println("Début du stream...");
 
           if(file.isOpen()) file.close();
-        }
-        
-        else if(currentMode == 1){
-  
-          Serial.println("Début du stockage des données...");
-          
-          // On vide les buffers avant de stocker les nouvelles données
+
+          // On vide les buffers pour stocker les nouvelles données
           activeBuf = 0;
           bufIdx = 0;
           fullFlag = false;
         }
-      }
-
-      // Mise à jour Bluetooth
-      if(currentMode == 0){ 
         
-        if(micros() - lastBleUpdate >= 500){
-          lastBleUpdate = micros();
-          ADXL_ReadRaw();
-
-          float ax = ((float)x_raw) / SCALE_FACTOR;
-          float ay = ((float)y_raw) / SCALE_FACTOR;
-          float az = ((float)z_raw) / SCALE_FACTOR;
-
-          float pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / PI;
-          float roll = atan2(ay, az) * 180.0 / PI;
+        else if(currentMode == RECORD){
+  
+          Serial.println("Début du stockage des données...");
           
-          xAcceloChar.writeValue(ax);
-          yAcceloChar.writeValue(ay);
-          zAcceloChar.writeValue(az);
-          pitchChar.writeValue(pitch); 
-          rollChar.writeValue(roll);
+          // On vide les buffers pour stocker les nouvelles données
+          activeBuf = 0;
+          bufIdx = 0;
+          fullFlag = false;
+
+          if (!file.isOpen()) {
+
+            snprintf(hours, sizeof(hours), "%02d", gps.time.hour());
+            snprintf(minutes, sizeof(minutes), "%02d", gps.time.minute());
+            snprintf(seconds, sizeof(seconds),"%02d", gps.time.second());
+
+            snprintf(title, sizeof(title), "dataRaw_%s_%s_%s_UTC.bin", hours, minutes, seconds);
+
+            if (!file.open(title, O_RDWR | O_CREAT | O_AT_END)) {
+              Serial.println("Echec critique : impossible de créer dataRaw.bin");
+            }
+            
+            else{
+              // On écrit une petite entête GPS au début du fichier
+              file.print("GPS:"); 
+              file.print(lastLat, 6); 
+              file.print(","); 
+              file.print(lastLon, 6);
+              file.print(",");
+
+              file.print(hours);
+              file.print(':');
+              file.print(minutes);
+              file.print(':');
+              file.print(seconds);
+              
+              file.println(); 
+            }
+          }
+          
         }
       }
-    }
+
+      // Mise à jour BLE
+      if(currentMode == LIVE && fullFlag){ 
+          int bufToSave = (activeBuf == 0) ? 2 : activeBuf - 1;
+          fullFlag = false;
+   
+          uint8_t* ptrBuf = (uint8_t*) bufferLIVE[bufToSave];
+
+          pRawDataChar->setValue(ptrBuf, 504);
+          pRawDataChar->notify();
+          pModeChar->setValue((uint8_t*) &currentMode, (size_t) sizeof(currentMode));
+      }
   }
 
   // Mode Enregistrement 
-  if(currentMode == 1){
-
-    if (fullFlag) {
-      int bufToSave = (activeBuf == 0) ? 1 : 0;
-
-      if (!file.isOpen()) {
-
-        if (!file.open("dataRaw.bin", O_RDWR | O_CREAT | O_AT_END)) {
-          Serial.println("Echec critique : impossible de créer dataRaw.bin");
-        }
-        
-        else{
-          // On écrit une petite entête GPS au début du fichier
-          file.print("GPS:"); 
-          file.print(lastLat, 6); 
-          file.print(","); 
-          file.print(lastLon, 6);
-          file.print(",");
-
-          // Formatage manuel de l'heure HH:MM:SS
-          if (gps.time.hour() < 10) file.print('0'); file.print(gps.time.hour());
-          file.print(':');
-          if (gps.time.minute() < 10) file.print('0'); file.print(gps.time.minute());
-          file.print(':');
-          if (gps.time.second() < 10) file.print('0'); file.print(gps.time.second());
-          
-          file.println(); // Fin de la ligne GPS
-        }
-      }
-      
-      if(file.isOpen()){
-        file.write((const uint8_t*)&buffer[bufToSave], sizeof(buffer[0]));
-        fullFlag = false;
-      }
-
+  if(currentMode == RECORD && fullFlag){
+    int bufToSave = (activeBuf == 0) ? 1 : 0;
+    
+    if(file.isOpen()){
+      file.write((const uint8_t*)&bufferSD[bufToSave], sizeof(bufferSD[0]));
+      fullFlag = false;
     }
+
+  }
+
+  static bool wasConnected = false;
+  // Gestion automatique de la reconnexion (Advertising)
+  if (!deviceConnected && wasConnected) {
+    delay(500); 
+    NimBLEDevice::startAdvertising();
+    wasConnected = false;
+  }
+  if (deviceConnected && !wasConnected) {
+    wasConnected = true;
   }
 }
 
--- SERVEUR_ADXL.py
+++ SERVEUR_ADXL.py
@@ -6,61 +6,79 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import RadioButtons
 import matplotlib.animation as animation
 from bleak import BleakClient, BleakScanner
+import math 
 
 # --- PARAMETRAGE ---
 DEVICE_NAME = "ADXL355Z" 
-UUID_X = "00002a58-0000-1000-8000-00805f9b34fb"
-UUID_Y = "00002a59-0000-1000-8000-00805f9b34fb"
-UUID_Z = "00002a5a-0000-1000-8000-00805f9b34fb"
-UUID_PITCH = "00002a5b-0000-1000-8000-00805f9b34fb"
-UUID_ROLL = "00002a5c-0000-1000-8000-00805f9b34fb"
-UUID_MODE = "00002a5d-0000-1000-8000-00805f9b34fb"
+UUID_RAWDATA = "19b10001-e8f2-537e-4f6c-d104768a1214"
+UUID_MODE = "19b10002-e8f2-537e-4f6c-d104768a1214"
+
+# Facteur d'échelle pour l'ADXL355 (+/- 2g) : 256 000 LSB/g
+SCALE_FACTOR = 256000.0
 
 PRECISION_STABILITE = 1     # +/- exprimée en °
 
-BUFFER_SIZE = 4000  # pour une frequence d'envoi de 4kHz
-x_data = deque([0.0]*BUFFER_SIZE, maxlen=BUFFER_SIZE)
-y_data = deque([0.0]*BUFFER_SIZE, maxlen=BUFFER_SIZE)
-z_data = deque([0.0]*BUFFER_SIZE, maxlen=BUFFER_SIZE)
+WINDOW_SIZE = 2000
+x_data = deque([0.0]*WINDOW_SIZE, maxlen=WINDOW_SIZE)
+y_data = deque([0.0]*WINDOW_SIZE, maxlen=WINDOW_SIZE)
+z_data = deque([0.0]*WINDOW_SIZE, maxlen=WINDOW_SIZE)
 latest_pitch = 0.0
 latest_roll = 0.0
 
 client_global = None
 ble_loop = None
 ble_running = True
-reconnect_delay = 3
+reconnect_delay = 5
 
 # --- BLE ---
 def notification_handler(sender, data):
-    """Reçoit les données BLE et met à jour les buffers en RAM directement."""
+    """Décode les nouvelles données reçues et les stocke dans les buffer"""
     global latest_pitch, latest_roll
 
-    val = struct.unpack('<f', data)[0]
     sender_uuid = sender.uuid.lower()
-    
-    if sender_uuid == UUID_X:
-        x_data.append(val)
-    elif sender_uuid == UUID_Y:
-        y_data.append(val)
-    elif sender_uuid == UUID_Z:
-        z_data.append(val)
-    elif sender_uuid == UUID_PITCH:
-        latest_pitch = val
-    elif sender_uuid == UUID_ROLL:
-        latest_roll = val
+
+    if sender_uuid == UUID_RAWDATA:
+        sample_size = 12
+        num_samples = len(data) // sample_size
+
+        for i in range(num_samples):
+            offset = i * sample_size
+            
+            x_raw, y_raw, z_raw = struct.unpack_from('<iii', data, offset)
+            
+            ax = x_raw / SCALE_FACTOR
+            ay = y_raw / SCALE_FACTOR
+            az = z_raw / SCALE_FACTOR
+            
+            x_data.append(ax)
+            y_data.append(ay)
+            z_data.append(az)
+
+            # On mémorise le dernier point du paquet pour actualiser les rotations X et Y
+            if i == num_samples - 1:
+                last_x, last_y, last_z = ax, ay, az
+        
+        try:
+            latest_roll = math.atan2(last_y, last_z) * 180.0 / math.pi
+            latest_pitch = math.atan2(-last_x, math.sqrt(last_y * last_y + last_z * last_z)) * 180.0 / math.pi
+
+        except ValueError:
+            # Sécurité en cas de division par zéro instable
+            pass
 
 async def run_ble():
     global client_global, ble_running
 
     while ble_running:
         print(f"Recherche de la carte '{DEVICE_NAME}'...")
-        
         device = await BleakScanner.find_device_by_name(DEVICE_NAME)
 
         if not device:
-            print("Carte introuvable. Nouvelle tentative dans 3 secondes...Fermez la fenêtre graphique pour quitter.")
-            await asyncio.sleep(3)
+            print("Carte introuvable. Nouvelle tentative dans 5 secondes...")
+            await asyncio.sleep(5)
             continue
+        
+        client = None
 
         try:
             client = BleakClient(device, disconnected_callback=on_disconnect)
@@ -69,11 +87,12 @@ async def run_ble():
 
             print("Connecté au BLE ! Démarrage du flux de données...")
 
-            await client.start_notify(UUID_X, notification_handler)
-            await client.start_notify(UUID_Y, notification_handler)
-            await client.start_notify(UUID_Z, notification_handler)
-            await client.start_notify(UUID_PITCH, notification_handler)
-            await client.start_notify(UUID_ROLL, notification_handler)
+            try:
+                await client.read_gatt_char(UUID_RAWDATA)
+            except Exception as mtu_error:
+                print(f"Avertissement MTU : {mtu_error}")
+
+            await client.start_notify(UUID_RAWDATA, notification_handler)
 
             while client.is_connected and ble_running:
                 await asyncio.sleep(1)
@@ -82,13 +101,16 @@ async def run_ble():
             print("Erreur BLE :", e)
 
         try:
-            if client.is_connected:
-                await client.disconnect()
-        except:
-            pass
+            if client is not None :
+                if client.is_connected:
+                    await client.disconnect()
+        
+        except Exception as disconnect_error:
+            print("Erreur lors de la déconnexion forcée :", disconnect_error)
+            
+        client_global = None
 
         if ble_running:
-            print(f"Tentative de reconnexion dans {reconnect_delay} secondes…")
             await asyncio.sleep(reconnect_delay)
 
     print("Arrêt complet du BLE.")
@@ -102,7 +124,7 @@ def change_mode(label):
         
     if client_global:
         asyncio.run_coroutine_threadsafe(
-            client_global.write_gatt_char(UUID_MODE, bytearray([val])), 
+            client_global.write_gatt_char(UUID_MODE, bytearray([val]), response=False), 
             ble_loop
         )
     print(f"Mode : {label}")
@@ -112,14 +134,14 @@ fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
 plt.subplots_adjust(left=0.25, hspace=0.4) 
 
 # Boutons de mode 
-rax = plt.axes([0.02, 0.7, 0.15, 0.15], facecolor='#f0f0f0')
+rax = plt.axes([0.02, 0.7, 0.15, 0.15], facecolor = '#f0f0f0')
 radio = RadioButtons(rax, ('Temps Réel', 'Enregistrer SD'))
 radio.on_clicked(change_mode)
 
 # Graphe des accélérations 
 ax1.set_title("Accélération (g)")
 ax1.set_ylim(-2.5, 2.5)
-ax1.set_xlim(0, BUFFER_SIZE)
+ax1.set_xlim(0, WINDOW_SIZE)
 line_x, = ax1.plot(x_data, label='X', color='red')
 line_y, = ax1.plot(y_data, label='Y', color='green')
 line_z, = ax1.plot(z_data, label='Z', color='blue')
@@ -132,6 +154,8 @@ text_roll  = ax2.text(0.3, 0.8, "Rotation X : 0.00°",  fontsize=15, ha='center'
 
 def update_plot(frame):
     """Mise à jour périodique des courbes avec le contenu actuel du buffer."""
+    global latest_pitch, latest_roll, activebuf, todisplay
+
     line_x.set_ydata(list(x_data))
     line_y.set_ydata(list(y_data))
     line_z.set_ydata(list(z_data))
@@ -162,7 +186,7 @@ if __name__ == "__main__":
 
     threading.Thread(target=start_ble, daemon=True).start()
     
-    ani = animation.FuncAnimation(fig, update_plot, interval=50, blit=True, cache_frame_data=False)
+    ani = animation.FuncAnimation(fig, update_plot, interval=10, blit=True, cache_frame_data=False)
 
     def on_close(event):
         global ble_running
