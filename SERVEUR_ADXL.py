190baa1103ef6c79cc412676243ff42b75aad9cb
Ad Bo <adambounour003@gmail.com>
2026-05-27 09:33:37 +0200

    Modification pour pouvoir paramétrer la plage de mesure ainsi que le type de sauvagarde + ajout d'un bouton pour mettre en pause la connection BLE (lors de la conversion des fichiers par exemple)

--- SERVEUR_ADXL.py
+++ SERVEUR_ADXL.py
@@ -1,24 +1,28 @@
+from collections import deque
+from matplotlib.widgets import RadioButtons, Button
+from bleak import BleakClient, BleakScanner
+from CONVERSION_BIN_CSV import process_batch
 import asyncio
 import threading
 import struct
-from collections import deque
 import matplotlib.pyplot as plt
-from matplotlib.widgets import RadioButtons
+import matplotlib.ticker as ticker
 import matplotlib.animation as animation
-from bleak import BleakClient, BleakScanner
 import math 
 
 # --- PARAMETRAGE ---
 DEVICE_NAME = "ADXL355Z" 
 UUID_RAWDATA = "19b10001-e8f2-537e-4f6c-d104768a1214"
 UUID_MODE = "19b10002-e8f2-537e-4f6c-d104768a1214"
-
-# Facteur d'échelle pour l'ADXL355 (+/- 2g) : 256 000 LSB/g
-SCALE_FACTOR = 256000.0
+UUID_RANGE = "19b10003-e8f2-537e-4f6c-d104768a1214"
 
 PRECISION_STABILITE = 1     # +/- exprimée en °
+SCALE_FACTORS = [256000.0, 128000.0, 64000.0]
+RANGE = 0
+WINDOW_SIZE = 3000
+SAVE_MODE = "CSV"
+BLE_ENABLE = True
 
-WINDOW_SIZE = 2000
 x_data = deque([0.0]*WINDOW_SIZE, maxlen=WINDOW_SIZE)
 y_data = deque([0.0]*WINDOW_SIZE, maxlen=WINDOW_SIZE)
 z_data = deque([0.0]*WINDOW_SIZE, maxlen=WINDOW_SIZE)
@@ -39,6 +43,7 @@ def notification_handler(sender, data):
 
     if sender_uuid == UUID_RAWDATA:
         sample_size = 12
+        last_x, last_y, last_z = (0,0,0)
         num_samples = len(data) // sample_size
 
         for i in range(num_samples):
@@ -46,9 +51,9 @@ def notification_handler(sender, data):
             
             x_raw, y_raw, z_raw = struct.unpack_from('<iii', data, offset)
             
-            ax = x_raw / SCALE_FACTOR
-            ay = y_raw / SCALE_FACTOR
-            az = z_raw / SCALE_FACTOR
+            ax = x_raw / SCALE_FACTORS[RANGE]
+            ay = y_raw / SCALE_FACTORS[RANGE]
+            az = z_raw / SCALE_FACTORS[RANGE]
             
             x_data.append(ax)
             y_data.append(ay)
@@ -67,9 +72,14 @@ def notification_handler(sender, data):
             pass
 
 async def run_ble():
-    global client_global, ble_running
+    global client_global, ble_running, BLE_ENABLE
 
     while ble_running:
+
+        if not BLE_ENABLE:
+            await asyncio.sleep(1)
+            continue
+
         print(f"Recherche de la carte '{DEVICE_NAME}'...")
         device = await BleakScanner.find_device_by_name(DEVICE_NAME)
 
@@ -87,13 +97,8 @@ async def run_ble():
 
             print("Connecté au BLE ! Démarrage du flux de données...")
 
-            try:
-                await client.read_gatt_char(UUID_RAWDATA)
-            except Exception as mtu_error:
-                print(f"Avertissement MTU : {mtu_error}")
-
             await client.start_notify(UUID_RAWDATA, notification_handler)
-
+            
             while client.is_connected and ble_running:
                 await asyncio.sleep(1)
 
@@ -127,38 +132,91 @@ def change_mode(label):
             client_global.write_gatt_char(UUID_MODE, bytearray([val]), response=False), 
             ble_loop
         )
-    print(f"Mode : {label}")
+
+def change_range(label):
+    global RANGE
+
+    dict_modes = {"2g": 0, "4g": 1, "8g": 2}
+    val = dict_modes[label]
+    RANGE = val
+
+    if client_global:
+        asyncio.run_coroutine_threadsafe(
+            client_global.write_gatt_char(UUID_RANGE, bytearray([val]), response=False), 
+            ble_loop
+        )
+
+def change_save(label):
+    global SAVE_MODE
+    SAVE_MODE = label
+
+def change_ble_state(label):
+    global BLE_ENABLE
+    BLE_ENABLE = (label == 'Activer')
+
+    if BLE_ENABLE:
+        print("\nRecherche Bluetooth relancée...\n")
+    else:
+        print("\nBluetooth désactivé. Déconnexion en cours...\n")
+
+def launch_extraction(event):
+    process_batch(SAVE_MODE)
 
 # --- Affichage (Matplotlib) ---
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6))
-plt.subplots_adjust(left=0.25, hspace=0.4) 
+fig, ax1 = plt.subplots(figsize=(10, 6))
+plt.subplots_adjust(left=0.25, right=0.75, bottom = 0.25) 
 
-# Boutons de mode 
-rax = plt.axes([0.02, 0.7, 0.15, 0.15], facecolor = '#f0f0f0')
-radio = RadioButtons(rax, ('Temps Réel', 'Enregistrer SD'))
-radio.on_clicked(change_mode)
+# Boutons  
+raxBLE = plt.axes([0.83, 0.82, 0.15, 0.12], facecolor="#babebe", title="Bluetooth")
+radioBLE = RadioButtons(raxBLE, ('Activer', 'Désactiver'))
+radioBLE.on_clicked(change_ble_state)
+
+raxMode = plt.axes([0.02, 0.82, 0.15, 0.12], facecolor = "#babebe", title="Modes")
+radioMode = RadioButtons(raxMode, ('Temps Réel', 'Enregistrer SD'))
+radioMode.on_clicked(change_mode)
+
+raxRange = plt.axes([0.02, 0.63, 0.15, 0.12], facecolor = "#babebe", title="Interval")
+radioRange = RadioButtons(raxRange, ('2g', '4g', '8g'))
+radioRange.on_clicked(change_range)
+
+raxFrequence = plt.axes([0.02, 0.44, 0.15, 0.12], facecolor = "#babebe", title="Frequence")
+radioFrequence = RadioButtons(raxFrequence, ('4000 Hz', '2000 Hz', '1000 Hz', '500 Hz'))
+
+raxSave = plt.axes([0.02, 0.25, 0.15, 0.12], facecolor = "#babebe", title="Sauvegarde PC")
+radioSave = RadioButtons(raxSave, ('CSV', 'BIN', 'CSV + BIN'))
+radioSave.on_clicked(change_save)
+
+axExtract = plt.axes([0.02, 0.05, 0.15, 0.05], facecolor = "#babebe")
+btnExtract = Button(axExtract, 'Extraire SD')
+btnExtract.on_clicked(launch_extraction)
 
 # Graphe des accélérations 
 ax1.set_title("Accélération (g)")
-ax1.set_ylim(-2.5, 2.5)
+ax1.set_xlabel("Echantillons")
+ax1.set_ylim(-8.5, 8.5)   
+ax1.yaxis.set_major_locator(ticker.MultipleLocator(1)) 
 ax1.set_xlim(0, WINDOW_SIZE)
 line_x, = ax1.plot(x_data, label='X', color='red')
 line_y, = ax1.plot(y_data, label='Y', color='green')
 line_z, = ax1.plot(z_data, label='Z', color='blue')
 ax1.legend(loc='upper right', fontsize='small')
 
+props = dict(boxstyle='round', facecolor="#fefefe")
+
 # Angles de rotation
-ax2.set_axis_off()
-text_pitch = ax2.text(0.7, 0.8, "Rotation Y : 0.00°", fontsize=15, ha='center', va='center', color='black', weight='regular')
-text_roll  = ax2.text(0.3, 0.8, "Rotation X : 0.00°",  fontsize=15, ha='center', va='center', color='black', weight='regular')
+ax2 = fig.add_axes([0.25, 0.05, 0.5, 0.15])
+ax2.axis('off')
+
+text_pitch = ax2.text(0.6, 0.5, "Rotation Y : 0.00°", fontsize=14, verticalalignment='top', bbox=props, transform=ax2.transAxes)
+text_roll  = ax2.text(0.1, 0.5, "Rotation X : 0.00°", fontsize=14, verticalalignment='top', bbox=props, transform=ax2.transAxes)
 
 def update_plot(frame):
     """Mise à jour périodique des courbes avec le contenu actuel du buffer."""
-    global latest_pitch, latest_roll, activebuf, todisplay
+    global latest_pitch, latest_roll, ax1
 
-    line_x.set_ydata(list(x_data))
-    line_y.set_ydata(list(y_data))
-    line_z.set_ydata(list(z_data))
+    line_x.set_ydata(list(x_data.copy()))
+    line_y.set_ydata(list(y_data.copy()))
+    line_z.set_ydata(list(z_data.copy()))
     text_pitch.set_text(f"Rotation Y : {latest_pitch:>.2f}°")
     text_roll.set_text(f"Rotation X : {latest_roll:>.2f}°")
     
