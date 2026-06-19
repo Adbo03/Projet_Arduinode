d487319f649c922849af8f49178aa055e564609f
Ad Bo <adambounour003@gmail.com>
2026-06-19 12:56:12 +0200

    Nettoyage du code

--- SOURCE_BLE.ino
+++ SOURCE_BLE.ino
@@ -13,18 +13,6 @@
 #define ON    1
 #define OFF   0
 
-// // Frequences
-// #define FREQ_10Hz   0
-// #define FREQ_20Hz   1
-// #define FREQ_30Hz   2
-// #define FREQ_40Hz   3
-// #define FREQ_50Hz   4
-// #define FREQ_60Hz   5
-// #define FREQ_70Hz   6
-// #define FREQ_80Hz   7
-// #define FREQ_90Hz   8
-// #define FREQ_100Hz  9
-
 const int tabHz[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 uint8_t sineTable[TABLE_SIZE];
 volatile uint8_t currentMode = OFF; 
@@ -42,8 +30,6 @@ volatile bool deviceConnected = false;
 bool wasConnected = false;
 volatile bool modeWritten = false;
 volatile bool freqWritten = false;
-
-//SemaphoreHandle_t timerSourceFlag;
 
 class MyServerCallbacks: public NimBLEServerCallbacks {
   public:
@@ -95,15 +81,6 @@ class FreqCallbacks: public NimBLECharacteristicCallbacks {
       }
 };
 
-
-// void IRAM_ATTR Source_handler() {
-//   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-//   xSemaphoreGiveFromISR(timerSampleFlag, &xHigherPriorityTaskWoken);
-//   if (xHigherPriorityTaskWoken) {
-//     portYIELD_FROM_ISR();
-//   }
-// }
-
 void SINE_Task(void* pv) { 
   int i = 0;
   unsigned long lastUpdate = 0;
@@ -113,7 +90,6 @@ void SINE_Task(void* pv) {
     if(currentMode == ON){
       unsigned long now = micros();
       
-      // Mise à jour de l'échantillon à l'intervalle calculé
       if(now - lastUpdate >= microsPerSample){
         lastUpdate = now;
         ledcWrite(0, sineTable[i]);             
@@ -128,18 +104,6 @@ void SINE_Task(void* pv) {
       i = 0;
       vTaskDelay(pdMS_TO_TICKS(50));
     }
-
-    // if (xSemaphoreTake(timerSourceFlag, portMAX_DELAY) == pdTRUE){
-    //   if(activeSource){
-    //     ledcWrite(0, sineTable[i]);
-    //     i++;
-    //     if(i > 255) i = 0;
-    //   } 
-      
-    //   else {
-    //     i = 0;
-    //   }
-    // }
   }
 }
 
@@ -155,14 +119,10 @@ void setupSineTable() {
 void setup() {
   Serial.begin(115200);
   delay(1000); 
-
-  // timerSourceFlag = xSemaphoreCreateBinary();
   
   xTaskCreatePinnedToCore(SINE_Task, "SINE_Task", 4096, NULL, 10, NULL, 1);
 
-  // Configuration BLE 
   NimBLEDevice::init("Nodequake"); 
-
   pServer = NimBLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
@@ -187,12 +147,6 @@ void setup() {
   pAdvertising->addServiceUUID(pService->getUUID());
   pAdvertising->setName("Nodequake");
   pAdvertising->start();
-
-  // Timer pour générer le sinus de la source
-  // timerSource = timerBegin(1, 80, true); 
-  // timerAttachInterrupt(timerSource, &Source_handler, true);
-  // timerAlarmWrite(timerSource, microsPerSample, true);
-  // timerAlarmEnable(timerSource);
   
   ledcSetup(0, 40000, SOURCE_RES); 
   ledcAttachPin(SOURCE_PIN, 0);
@@ -226,16 +180,12 @@ void loop() {
       
       samplesPerSec = (float) TABLE_SIZE * (float) tabHz[currentFreq];
       microsPerSample = (unsigned long)(1000000.0 / samplesPerSec);
-
-      // timerAlarmDisable(timerSource);
-      // timerAlarmWrite(timerSource, microsPerSample, true);
-      // timerAlarmEnable(timerSource);
       
       pFrequencyChar->setValue((uint8_t*) &currentFreq, (size_t) sizeof(currentFreq));    
     }
   }
 
-  // Gestion automatique de la reconnexion (Advertising)
+  // Gestion automatique de la reconnexion
   if (!deviceConnected && wasConnected) {
     delay(500); 
     NimBLEDevice::startAdvertising();
